6d1ff5fe902d28b52194ddf8aca83f8998d8c96d
LuccaRendall <luccarendall2@gmail.com>
2023-09-13 19:16:02 -0300

    feat: componente genérico

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import MeuComponente from './components/useState'; // adaptar para cada hook
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MeuComponente />
     </div>
   );
 }
---
+++ src/components/useState.js
@@ -0,0 +1,13 @@
+// TemplateComponent.js
+import React from 'react';
+
+function TemplateComponent() {
+  return (
+    <div>
+      <h1>Meu Template Component</h1>
+      <p>Este é um componente de template.</p>
+    </div>
+  );
+}
+
+export default TemplateComponent;
