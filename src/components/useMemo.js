e043d51103017579560ff196a6fc1487567d777d
LuccaRendall <luccarendall2@gmail.com>
2023-09-13 21:02:07 -0300

    Feat: Versão sem useMemo - 6 a 8 segundos para responder

---
+++ src/components/useMemo.js
@@ -0,0 +1,35 @@
+import React, {useState, useEffect, useMemo} from 'react';
+
+// O useMemo é um hook que trabalha com memoização (sim, tá certo). É uma técnica focada em melhorar o desempenho de softwares, que consiste em armazenar em cache/memória o resultado de chamadas à funções específicas que sejam custosas. Geralmente é usada para funções que retornam o mesmo valor várias vezes e/ou é uma função que demora para ser computada, pra buscar os dados de uma requisição, enfim... uma função que demora e pesa o fluxo.
+
+function MeuComponente() {
+  const [num1, setNum1] = useState(1);
+  const [num2, setNum2] = useState(1);
+  const [num3, setNum3] = useState(1);
+  const [num4, setNum4] = useState(1);
+
+  const potencia = () => {
+    const future = Date.now() + 1000;
+    while (Date.now() < future) {}
+    return num1 ** num2;
+  }
+
+  const soma = num3 + num4;
+
+  return (
+    <div className='main'>
+        <p> {`${num1}^${num2}: ${potencia()}`} </p>
+        <div className='buttons'>
+          <input type='number' value={num1} onChange={ (e) => setNum1(Number(e.target.value)) } />
+          <input type='number' value={num2} onChange={ (e) => setNum2(Number(e.target.value)) } />
+        </div>
+        <p> {`${num3}+${num4}: ${soma}`} </p>
+        <div className='buttons'>
+          <input type='number' value={num3} onChange={ (e) => setNum3(Number(e.target.value)) } />
+          <input type='number' value={num4} onChange={ (e) => setNum4(Number(e.target.value)) } />
+        </div>
+    </div>
+  );
+}
+
+export default MeuComponente;
