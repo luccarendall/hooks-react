2f80ad0939c5ec427833c72376c5573f7f6ced7e
LuccaRendall <luccarendall2@gmail.com>
2023-09-15 18:46:36 -0300

    feat: criação da constante com o useReducer e sintaxe

--- src/components/useReducer.js
+++ src/components/useReducer.js
@@ -1,10 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useReducer } from 'react';
 
-// Alternativa ao useState, permite criar variáveis que ao serem modificadas a pagina renderiza novamente com os novos valores. Funciona como um useState só que gerenciando várias variáveis de  estado ao mesmo tempo
 
 function TemplateComponent() {
   const [count, setCount] = useState(0);
   const [showText, setShowText] = useState(true);
+
+
+// Sintaxe:
+  // const [{A}, B] = useReducer (C, {D})
+  // A = obj com os estados
+  // B = dispatch --> é a função que dispara uma ação(como o setState)
+  // C = Função que diz o que vai acontecer a cada vez que a gente despachar alguma coisa
+  // D = obj com os valores dos estados iniciais
+  const [state, dispatch] = useReducer(reducer, {
+    count:0,
+    showText: true,
+  });
 
 
 
@@ -15,10 +26,12 @@ function TemplateComponent() {
     // useEffect(() => { // Esse useEffect gera um loop que atualiza os estados infinitamente. Ao fazer cerca de 16 mil atualizações, a implementação com prevCount conta todas enquanto a atualização direto no estado count perdeu 1 atualização. Num cenário com 1 milhão de requisições isso geraria um erro de quase 63 no valor real do estado
     //   // setCout(count + 1) --> implementação ruim
     //   // setCout2((prevCount) => prevCount + 1) --> implementação melhor
+    // Isso é chamado de "função de atualização de estado" e é útil em situações de concorrência, pois garante que você esteja atualizando o estado com base no valor mais recente, mesmo se várias atualizações estiverem ocorrendo simultaneamente. Isso pode ajudar a evitar problemas de perda de atualizações.
     // })
 
-  const handleClick = () => {
-    setCount((prevCount) => prevCount + 1) // Isso é chamado de "função de atualização de estado" e é útil em situações de concorrência, pois garante que você esteja atualizando o estado com base no valor mais recente, mesmo se várias atualizações estiverem ocorrendo simultaneamente. Isso pode ajudar a evitar problemas de perda de atualizações.
+  const handleClick = () => { // Nesse caso que temos 1 função atualizando 2 ou + estados podemos usar o userReducer
+    setCount((prevCount) => prevCount + 1)
+    setShowText(!showText) // inverte o valor
   }
 
 
