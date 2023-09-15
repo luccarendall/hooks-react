2549438ebe8adab9a49f4b9ae5aac05b5c90c96d
LuccaRendall <luccarendall2@gmail.com>
2023-09-14 22:17:35 -0300

    feat: configuração de rotas pronta usando o createBrowserRouter - faltando prover para a aplicação

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+import Home from './components/routes/Home'
+import Contact from './components/routes/Contact'
+
+const router = createBrowserRouter([ // sintaxe router = createBrowserRouter([{}]) - recebe um array de objetos com o componente base - 'path: '/' element: <App /> - e as demais páginas (componentes) vem como children do componente base (App.js)
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {path: '/', element: <Home />},
+      {path: '/contact', element: <Contact />},
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
