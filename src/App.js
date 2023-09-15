717824e1b9b55107e2706df44e839e4a2260cf4c
LuccaRendall <luccarendall2@gmail.com>
2023-09-14 22:23:46 -0300

    feat: Acessando componentes pelo App usando o Outlet do react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import MeuComponente from './components/useContext'; // adaptar para cada hook
 import './App.css';
+
+import { Outlet } from 'react-router-dom';
+// Com o Outlet do react-router-dom conseguimos exibir as páginas que foram providas à aplicação (lá no index.js)
 
 function App() {
   return (
     <div className="App">
-      <MeuComponente />
+      <Outlet />
     </div>
   );
 }
