7224a0f14dffe746a212b8e3b887f3177eb1dfb1
LuccaRendall <luccarendall2@gmail.com>
2023-09-15 13:01:25 -0300

    feat: Criando arquivo para o Context e setando o provider envolvendo children

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext()
+
+export const ThemeProvider = ({children}) => {
+  return (
+    <ThemeContext.Provider>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
