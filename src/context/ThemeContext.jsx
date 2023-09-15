473b6d164f12ce8579a1ca650d2763f7b47a8d74
LuccaRendall <luccarendall2@gmail.com>
2023-09-15 13:07:11 -0300

    feat: Contexto e provider criados, como compartilhar isso com a aplicação? Resposta no próximo commit

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -3,8 +3,19 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext()
 
 export const ThemeProvider = ({children}) => {
+  const [theme, setTheme] = useState('light');
+
+  const modifyTheme = () => { // função para alterar o tema
+    if( theme === 'light') {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }
+
   return (
-    <ThemeContext.Provider>
+    // passar na variável value os valores que eu quero compartilhar entre os componentes
+    <ThemeContext.Provider value={{theme, modifyTheme}}> {/* nesse caso estou compartilhando uma forma de consultar o estado atual do tema e uma forma de alterar o estado do tema */}
       {children}
     </ThemeContext.Provider>
   )
