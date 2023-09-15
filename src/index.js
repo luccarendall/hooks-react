ef2f63442c45a626d65b3740429a48d26eafa472
LuccaRendall <luccarendall2@gmail.com>
2023-09-14 22:20:03 -0300

    feat: Provendo rotas para a aplicação

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ const router = createBrowserRouter([ // sintaxe router = createBrowserRouter([{}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App />  No lugar do App deve colocar RouterProvider e passar a prop router recebendo router */}
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
